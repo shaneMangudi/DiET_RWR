d969c9266639b19cccbe4fd18721e9b464c38b40
Zacharias Markakis <zacfer@gmail.com>
2016-09-19 20:10:34 +0100

    Change static random mapping and fix ZOMG bug

--- src/diet/server/ConversationController/rwr/CardMappings.java
+++ src/diet/server/ConversationController/rwr/CardMappings.java
@@ -3,7 +3,7 @@ package diet.server.ConversationController.rwr;
 import java.util.*;
 
 public class CardMappings {
-    private static final List<Integer> STATIC_RANDOM_MAPPING = Arrays.asList(6, 1, 4, 3, 2, 5, 8, 7);
+    private static final List<Integer> STATIC_RANDOM_MAPPING = Arrays.asList(5, 1, 4, 3, 2, 7, 8, 6);
 
     private final Map<Integer, Integer> idMappings;
 
@@ -14,12 +14,12 @@ public class CardMappings {
     public static CardMappings from(List<Integer> cardIds, CardMappingType cardMappingType) {
         if (cardMappingType == CardMappingType.DIRECT) {
             return new CardMappings(zip(cardIds, cardIds));
-        } else if (cardMappingType == CardMappingType.RANDOM || cardMappingType == CardMappingType.STATIC_RANDOM) {
+        } else if (cardMappingType == CardMappingType.RANDOM || cardMappingType == CardMappingType.PSEUDO_RANDOM) {
             List<Integer> copyOfCardIds = new ArrayList<>(cardIds);
             Random random = cardMappingType == CardMappingType.RANDOM ? new Random() : new Random(0L);
             Collections.shuffle(copyOfCardIds, random);
             return new CardMappings(zip(cardIds, copyOfCardIds));
-        } else if (cardMappingType == CardMappingType.PSEUDO_RANDOM) {
+        } else if (cardMappingType == CardMappingType.STATIC_RANDOM) {
             if (cardIds.size() == 8 || cardIds.size() == 6) {
                 return new CardMappings(zip(cardIds, STATIC_RANDOM_MAPPING.subList(0, cardIds.size())));
             }
